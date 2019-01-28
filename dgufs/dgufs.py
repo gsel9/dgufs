df6c55140699207f3cbc52685319bc977b50aed8
SeverinLangberg <Langberg91@gmail.com>
2019-01-28 10:39:52 +0100

    Added speed up fucntion (SCAMS)

--- dgufs/dgufs.py
+++ dgufs/dgufs.py
@@ -4,6 +4,7 @@ import numpy as np
 
 import utils
 
+from scipy.linalg import eig
 from scipy.spatial import distance
 
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -32,9 +33,9 @@ class DGUFS(BaseEstimator, TransformerMixin):
         self,
         num_features=2,
         num_clusters=2,
-        num_neighbors=4,
+        num_neighbors=2,
         alpha=0.5,
-        beta=0.5,
+        beta=0.9,
         tol=5e-7,
         max_iter=1e2,
         mu=1e-6,
@@ -63,7 +64,6 @@ class DGUFS(BaseEstimator, TransformerMixin):
         if self.num_features > ncols:
             raise ValueError('Number of features to select exceeds the number '
                              'of columns in X ({})'.format(ncols))
-
         if nrows < 2:
             raise RuntimeError('Feature selection requires more than two '
                                'samples')
@@ -88,13 +88,7 @@ class DGUFS(BaseEstimator, TransformerMixin):
 
 
         # TEMP:
-        rho = 1.1
-        max_mu = 1e10
-        mu = 1e-6
-        # maximum number of iterations
         max_iter = 1#1e2
-        # the other stop criterion for iteration
-        tol = 5e-7
 
         i = 1
         while i <= max_iter:
@@ -109,8 +103,29 @@ class DGUFS(BaseEstimator, TransformerMixin):
 
             # Update L.
             speed_up = self.speed_up(H.dot(np.transpose(Y)).dot(Z).dot(H))
-            temp2 = ((1 - self.beta) * speed_up + self.beta * S - Lamda2) / self.mu + M
+            U2 = ((1 - self.beta) * speed_up + self.beta * S - Lamda2) / self.mu + M
 
+            A = self.speed_up(U2)
+            eta = 2 * self.alpha / self.mu
+
+            A = 0.5 * (A + np.transpose(A))
+            tempD, tempV = eig(A)
+            # Cast complex values to real discarding the imaginary part.
+            tmpD = tempD.astype(float)
+            tempD = np.diag(tempD).astype(float)
+
+            # eta * rank(P)
+            tmpD[tmpD <= np.sqrt(eta)] = 0
+            tempD = np.diag(tmpD)
+            
+            print(tempD)
+
+            """
+            tmpD(tmpD<=sqrt(eta)) = 0; % eta*rank(P)
+            tempD = diag(tmpD);
+            P = tempV*tempD*tempV';
+
+            """
             #L = solve_rank_lagrange(speedUp(temp2), 2*alpha/mu);
 
             i = i + 1
@@ -125,6 +140,7 @@ class DGUFS(BaseEstimator, TransformerMixin):
         # Update Z:
         # Optimal (X - Z) with algorithm 1. Update Z by using the definition of U.
 
+    @staticmethod
     def similarity_matrix(X):
 
         S = distance.squareform(distance.pdist(np.transpose(X)))
@@ -142,7 +158,32 @@ class DGUFS(BaseEstimator, TransformerMixin):
 
         return P
 
-    def speed_up(self):
+    def speed_up(self, C):
+        """Refer to Simultaneous Clustering and Model Selection (SCAMS),
+        CVPR2014.
+
+        """
+        diagmask = np.eye(np.shape(C)[0], dtype=bool)
+        # Main diagonal = 0.
+        C[diagmask] = 0
+
+        # If C is (N x N), then tmp is (N*N x 1).
+        tmp = np.reshape(C, (np.size(C), 1))
+        # Remove the main diagonal elements of C in tmp. Then tmp has a
+        # length of N * (N - 1).
+        tmp = np.delete(tmp, np.where(diagmask.ravel()))
+        # Scale to [0, 1] range.
+        tmp = (tmp - np.min(tmp)) / (np.max(tmp) - np.min(tmp))
+
+        affmaxo = C
+        # affmaxo(~diagmask) is a column vector.
+        affmaxo[np.logical_not(diagmask)] = tmp
+        C_new = affmaxo
+
+        return C_new
+
+    def solve_rank_lagrange(self, A, eta):
+        #return P
         pass
 
     def transform(self, X, y=None, **kwargs):
@@ -155,7 +196,5 @@ if __name__ == '__main__':
     X = np.array(
         [[ 1, -4, 22], [12,  4,  0], [12,  0, -2], [12,  15, -2], [9,  3, 0]]
     ).T
-    #dgufs = DGUFS()
-    #dgufs.fit(X)
-
-    print(X)
+    dgufs = DGUFS()
+    dgufs.fit(X)
