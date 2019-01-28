5e27d5eda8e044e92fc39c16b78a3f1110a4521d
SeverinLangberg <Langberg91@gmail.com>
2019-01-28 13:26:42 +0100

    updated init

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# __init__.py
+#
+
+"""Initializer of dgufs."""
+
+__author__ = 'Severin E. R. Langberg'
+__email__ = 'Langberg91@gmail.no'
+
+
+from .dgufs import utils
+from .dgufs import dgufs
